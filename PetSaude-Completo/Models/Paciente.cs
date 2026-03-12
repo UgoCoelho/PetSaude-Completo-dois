47d7f840236ceefac8a31ce524ee1709e874c060
UGO COELHO <2024102070@academicougb.com.br>
2026-03-12 16:09:31 -0300

    adicionamos a tabela de pacientes

---
+++ PetSaude-Completo/Models/Paciente.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace PetSaude_Completo.Models
+{
+    public class Paciente
+    {
+        public int IDPaciente { get; set; }
+        public string? Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string? Sexo { get; set; }
+        public string? Raca { get; set; }
+        public string? Cpf { get; set; }
+        public string? Cns { get; set; }
+        public string? Celular { get; set; }
+        public string? Microarea { get; set; }
+
+    }
+}
